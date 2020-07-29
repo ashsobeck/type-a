7d02123700fe798c85207696e1021507b72f43dc
Ashton Sobeck <asobeck@g.clemson.edu>
2020-07-29 17:35:22 -0400

    added accuracy component for front end

--- src/Accuracy/index.jsx
+++ src/Accuracy/index.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import { Box, Text } from "@chakra-ui/core";
 
 const Accuracy = (props) => {
+  const accStr = `  ${props.acc} | acc`;
   return (
-    <Box fontSize="sm" my="5" h="2rem" alignContent="center">
-      <Text my="2" direction="ltr" textAlign="center">
-        {props.acc} | acc
+    <Box id="acc" fontSize="sm" my="5" h="2.2rem" alignContent="flex-end">
+      <Text my="2" direction="ltr" textAlign="left">
+        {accStr}
       </Text>
     </Box>
   );
